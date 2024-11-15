1cf089fabb9b374128606e803b0d817e5a730c29
Chris Conlon <chris@wolfssl.com>
2024-11-14 17:39:04 -0700

    JNI/JSSE: wrap wolfSSL_DisableExtendedMasterSecret(), add support for System property jdk.tls.useExtendedMasterSecret

--- src/java/com/wolfssl/WolfSSLSession.java
+++ src/java/com/wolfssl/WolfSSLSession.java
@@ -352,6 +352,7 @@ public class WolfSSLSession {
     private native int rehandshake(long ssl);
     private native int set1SigAlgsList(long ssl, String list);
     private native int useSupportedCurve(long ssl, int name);
+    private native int disableExtendedMasterSecret(long ssl);
     private native int hasTicket(long session);
     private native int setMTU(long ssl, int mtu);
     private native String stateStringLong(long ssl);
@@ -1742,6 +1743,23 @@ public class WolfSSLSession {
         }
 
         return ret;
+    }
+
+    /**
+     * Disable TLS Extended Master Secret usage.
+     *
+     * @return <code>WolfSSL.SSL_SUCCESS</code> on success, otherwise
+     *         negative on error.
+     * @throws IllegalStateException WolfSSLSession has been freed
+     */
+    public int disableExtendedMasterSecret()
+        throws IllegalStateException {
+
+        confirmObjectIsActive();
+
+        synchronized (sslLock) {
+            return disableExtendedMasterSecret(this.sslPtr);
+        }
     }
 
     /* ---------------- Nonblocking DTLS helper functions  -------------- */
